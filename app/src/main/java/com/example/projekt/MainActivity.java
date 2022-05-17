1d9a6df81cbb31b4c15f9ba124734b42a4e109cd
a21hugni <a21hugni@student.his.se>
2022-05-17 16:17:30 +0200

    skapade en about activity

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a21hugni";
+    private RecyclerView recyclerView;
+    private List<Grejer> listan;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<Grejer>(){}.getType();
+        ArrayList<Grejer> skit = gson.fromJson(json, type);
+        mountainsList.addAll(skit);
+        adapter.notifyDataSetChanged();
     }
 }
